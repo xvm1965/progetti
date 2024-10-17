b332e2f3261a78d557ea27e8ac25ed930a70e33d
xvm1965 <159060973+xvm1965@users.noreply.github.com>
2024-10-17 12:06:10 +0200

    Update settings.py

--- flowe/settings.py
+++ flowe/settings.py
@@ -157,7 +157,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "normativa/static"),
     os.path.join(BASE_DIR, "normativa", "static"),
 ]
-
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
